0202200d0ee827f672d2c85bdf7f740b1caf649b
Andrew Brought <andrewbrought@gmail.com>
2020-10-20 10:44:31 -0500

    intro to working with functions in clojure

--- src/tutorial/fundamentals.clj
+++ src/tutorial/fundamentals.clj
@@ -13,6 +13,47 @@
          (+ 2 3)
          )
 
+(def names (str/split-lines (slurp "/usr/share/dict/propernames")))
+
+; Practicing Simple Funtions
+
+; Here we have a function that will join the characters with
+; a '-' and reverse the order ** to use with REPL
+; we first have to run the line (shift + cmd + p) then we
+; can call the function in the REPL window
+(def mangle
+  (fn [string]
+    (str/join "-" (str/reverse string))))
+
+; Another way to write this function is to shorten by including
+; defn
+(defn mangle2
+  [string]
+  (str/join "*" (str/reverse string)))
+
+; This will reverse a reversed string
+(defn de-mangle
+  [mangled-string]
+  (str/reverse (str/replace mangled-string "-" "")))
+
+(defn de-mangle2
+  [mangled-string]
+  (str/reverse (str/replace mangled-string "*" "")))
+
+; In this function we want to determine if a word is a
+; palindrome - we use the ? to give it a boolean value
+; We use "let" to create some temporary variables only inside this scope
+(defn palindrome? [word]
+  (let [lower-case (str/lower-case word)
+        reversed (str/reverse lower-case)]
+    ; This is stating: if the reversed lower-case version of the word is equal
+    ; to the plain lower-cased version, return true/else false
+    (= reversed lower-case)))
+
+
+
+
+
 (defn -main
   [& args]
 
